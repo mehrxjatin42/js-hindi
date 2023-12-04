440985d37bdaa85a36448e530e87a610f81effb8
Jatin Mehra <110673393+mehrxjatin42@users.noreply.github.com>
2023-12-04 04:07:12 +0000

    01_variables added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553;
+let accountEmail = "abc@hmail.com";
+var accoundPassword = "12345";
+accountCity = "Jaipur";
+let accountState;
+// accountId = 2; // not allowed
+
+accountEmail = "accb@mail.cpm";
+accoundPassword = "21212121";
+accountCity = "Bangaluru"
+
+console.log(accountId);
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accoundPassword,accountCity,accountState])
